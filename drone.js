925ef4a333c1ab08372e7c87d8902aaa163ec056
tanguy Colou <tanguy.colou@skema.edu>
2018-09-08 18:21:39 +0200

    added HTML and CSS

--- drone.js
+++ drone.js
@@ -1,1 +1,14 @@
+
+
 console.log("nique ta mère")
+
+
+console.log('Hello World from Webpacker')
+
+  $('.menu__link').on('click', function(e) {
+
+    e.preventDefault;
+  $(this).toggleClass('menu__link--active');
+  $('.nav').toggleClass('visible');
+
+});
