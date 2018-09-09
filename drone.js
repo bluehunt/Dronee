7f215c2ed410d7fa973bb909d80185c840347b83
Valentin Raille <Valrail78@gmail.com>
2018-09-09 20:23:55 +0200

    dropdown done

---
+++ drone.js
@@ -0,0 +1,7 @@
+console.log("hello")
+const menu = document.getElementById('menu');
+const list = document.getElementById('list');
+menu.addEventListener("click", (event) => {
+  list.classList.toggle("hidden")
+  list.classList.toggle("show")
+});
