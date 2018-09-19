0add1b63755e2309c76d073279bfdd8ad9c7eb8c
Valentin Raille <Valrail78@gmail.com>
2018-09-19 19:36:46 +0200

    finish transition

--- drone.js
+++ drone.js
@@ -1,12 +1,9 @@
 console.log("hello")
 const menu = document.getElementById('menu');
-const toggle = document.getElementById('toggle');
+const toggle = document.querySelector('.toggle');
 menu.addEventListener("click", (event) => {
-  if (toggle.innerHTML == "<ul><li>Home</li><li>Work</li><li>Contact</li></ul>") {
-   toggle.innerHTML = "<ul style='opacity: 1;'><li>Home</li><li>Work</li><li>Contact</li></ul>"
-  } else {
-    toggle.innerHTML = "<ul><li>Home</li><li>Work</li><li>Contact</li></ul>"
-  }
+  toggle.classList.toggle("showm");
+  toggle.classList.toggle("");
 });
 
 
