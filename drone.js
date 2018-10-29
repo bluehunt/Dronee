2350f460715eebf86cff4da809221a0a3a256994
Valentin Raille <Valrail78@gmail.com>
2018-10-29 11:08:40 +0100

    Et c'est repartit

--- drone.js
+++ drone.js
@@ -1,9 +1,20 @@
-console.log("hello")
-const menu = document.getElementById('menu');
-const toggle = document.querySelector('.toggle');
-menu.addEventListener("click", (event) => {
-  toggle.classList.toggle("showm");
-  toggle.classList.toggle("");
-});
+var arr = ["Clément-Truff","Dudu","whillish","L'artiste", "Bénichou", "Valiche"];
+
+var arrbeuh = [0,1,2,3,4,5];
 
 
+function Départagement () {
+
+
+    // var test = arrbeuh[Math.floor(Math.random() * 6)];
+    // var plus = Math.floor(Math.random() * 6)
+    // var expé = arr[plus];
+
+    // arrbeuh.splice(test, 1);
+    // arr.splice(plus, 1);
+    console.log(arr.sort());
+    console.log(arrbeuh.sort());
+
+}
+
+Départagement();
