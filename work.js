78893d2caa2542ef3071f297bc1614f3386d8753
Valentin Raille <Valrail78@gmail.com>
2018-12-07 16:54:10 +0100

    deploying for real

--- work.js
+++ work.js
@@ -1,31 +1,13 @@
-const menu1 = document.getElementById('navbar1');
-const contact1 = document.getElementById('card1');
-const list1 = document.getElementById('list-container1');
-const contactTarget1 = document.getElementById('contact-target1');
+console.log("hello")
+const menu = document.getElementById('navbar');
+const list = document.getElementById('list-container');
+const contact = document.getElementById('card');
+const contactTarget = document.getElementById('contact-target');
+
+
+contactTarget.addEventListener("click", function() {
+  contact.classList.toggle('contact-work-view');
+})
 
 
 
-
-menu1.addEventListener("click", (event) => {
-
-  if (menu1.src === 'https://res.cloudinary.com/ebc-industries/image/upload/v1534499757/DRopdownicone.png') {
-
-    list1.style.display = 'block';
-    window.setTimeout(function() {
-      list1.classList.toggle('pascaché');
-      menu1.src = 'https://res.cloudinary.com/ebc-industries/image/upload/v1540041871/croixicone.png';
-    }, 15);
-
-  } else {
-  list1.classList.toggle('pascaché');
-  menu1.src = 'https://res.cloudinary.com/ebc-industries/image/upload/v1534499757/DRopdownicone.png';
-  window.setTimeout(function(){list1.style.display = 'none'}, 700);
-  }
-
-});
-
-
-
-contactTarget1.addEventListener("click", function() {
-  contact1.classList.toggle('contact-work-view');
-});
